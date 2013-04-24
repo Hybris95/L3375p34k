126cca88d72103c6b884e69dddc093c9e41bba45
Hybris95 <hybris_95@hotmail.com>
2013-04-24 07:23:40 -0400

    Added GNU GPL3 Licence

--- src/com/hybris/bukkit/l3375p34k/L3375p34kExecutor.java
+++ src/com/hybris/bukkit/l3375p34k/L3375p34kExecutor.java
@@ -1,3 +1,20 @@
+/*
+    L3375p34k - The plugin for CraftBukkit that allows you to talk in Leet
+    Copyright (C) 2013  Hybris95
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.hybris.bukkit.l3375p34k;
 
 import org.bukkit.command.CommandExecutor;
