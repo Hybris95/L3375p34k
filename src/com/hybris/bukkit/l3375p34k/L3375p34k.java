d87495b15f703e8e6a6eda6f7bf00b6c447fdd70
Hybris95 <hybris_95@hotmail.com>
2013-04-24 07:51:07 -0400

    Added my mail address into the licence

--- src/com/hybris/bukkit/l3375p34k/L3375p34k.java
+++ src/com/hybris/bukkit/l3375p34k/L3375p34k.java
@@ -1,6 +1,7 @@
 /*
     L3375p34k - The plugin for CraftBukkit that allows you to talk in Leet
     Copyright (C) 2013  Hybris95
+    hybris_95@hotmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
